15f7289d2dcb6d4453e6e61f5744ab2d07dde60a
Thomas Kekeisen <thomas@thomaskekeisen.de>
2015-09-26 17:15:03 +0200

    Initial commit

---
+++ OHFTheMenTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OHFTheMenTestApp
+//
+//  Created by Thomas Kekeisen on 26/09/15.
+//  Copyright © 2015 Socialbit GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OHFTheMenTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OHFTheMenTestApp
+//
+//  Created by Thomas Kekeisen on 26/09/15.
+//  Copyright © 2015 Socialbit GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
